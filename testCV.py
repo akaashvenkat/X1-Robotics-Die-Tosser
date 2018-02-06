0585fda35c5e0c1704488084bc3120213fd65eb7
Craig Young <young.craigj12@gmail.com>
2018-02-05 16:56:34 -0800

    Update testCV.py

--- testCV.py
+++ testCV.py
@@ -40,7 +40,7 @@ cv2.imwrite('img2.jpg', img2)
 
 IMAGE_PATHS = ['2.jpg','3.jpg', '4.jpg']
 
-img1 = cv2.imread('2.jpg')
+img1 = cv2.imread(IMAGE_PATHS[0])
 img1S = cv2.resize(img1, (756,1008))
 print 'Calibrating Known Image'
 marker = find_marker(img1S)
@@ -89,20 +89,3 @@ for imagePath in IMAGE_PATHS:
     cv2.destroyAllWindows()
     count = count + 1
 
-# for imagePath in IMAGE_PATHS:
-#     image = cv2.imread(imagePath)
-#     # load the image, find the marker in the image, then compute the
-#     # distance to the marker from the camera
-#     #image = cv2.imread(imagePath)
-#     marker = find_marker(image)
-#     inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
-#
-# 	# draw a bounding box around the image and display it
-# 	#box = np.int0(cv2.cv.BoxPoints(marker))
-# 	#cv2.drawContours(image, [box], -1, (0, 255, 0), 2)
-#     image = cv2.polylines(img, marker, True, (0,255,255))
-#     cv2.putText(image, "%.2fft" % (inches / 12),
-# 		(image.shape[1] - 200, image.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,
-# 		2.0, (0, 255, 0), 3)
-#         cv2.imshow("image", image)
-#         cv2.waitKey(0)
